1f9a8dc74717041dd516af4cc9356e1051629fb3
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-05-24 20:57:14 +0300

    connect to atlas

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const User = require('./User');
-
-mongoose.connect('mongodb://127.0.0.1:27017/appdb', {
+const url = "mongodb+srv://mongo-admin:pass@cluster0.b0wdnst.mongodb.net/todoListDB";
+//mongodb-admin = username, pass = password, todolistDB = db name
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true})
-  .then(console.log('connection to db is successfull'));
+  .then(console.log('mongoose connection to db is successfull'));
 
 const user = new User({ name: 'Slav1', age: 29 });
 user.save()
-  .then(() => console.log('user saved'))
+  .then(() => console.log('mongoose user saved'))
   .catch((error) => console.error(error))
   .finally(() => mongoose.disconnect());
